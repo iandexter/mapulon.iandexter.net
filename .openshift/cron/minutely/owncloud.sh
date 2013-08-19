1965f83859a14cc2307ec097cf5211e55baed7a7
Ian Dexter R Marquez <iandexter+tech@gmail.com>
2013-08-19 14:28:13 +0800

    Add cron job.

---
+++ .openshift/cron/minutely/owncloud.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+ 
+# Execute background job every 15 minutes
+
+if [[ -f  $OPENSHIFT_REPO_DIR/php/cron.php ]] ; then 
+    if [[ $(( $(date +%M) % 15 )) -eq 0 ]] ; then
+        printf "{\"app\":\"Cron\",\"message\":\"%s\",\"level\":1,\"time\":%s}\n" "Running cron job" $(date +%s) >> $OPENSHIFT_DATA_DIR/owncloud.log
+        pushd $OPENSHIFT_REPO_DIR/php &> /dev/null
+        php -f cron.php
+	if [[ $? -ne 0 ]] ; then
+        	printf "{\"app\":\"Cron\",\"message\":\"%s\",\"level\":2,\"time\":%s}\n" "Error running cron job" $(date +%s) >> $OPENSHIFT_DATA_DIR/owncloud.log
+	fi
+        popd &> /dev/null
+    fi
+fi
